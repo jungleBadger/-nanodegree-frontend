15205328c515c47ae42407516d2603dced3f914e
Daniel Abrão <danielpaladar@gmail.com>
2017-11-13 18:11:13 -0200

    adding default port to node

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
     const express = require("express");
     const app = express();
-	const appPort = process.env.APP_PORT || 6050;
+	const appPort = process.env.APP_PORT || process.env.PORT || 6050;
 	const server = require("http").createServer(app);
 	// const io = require("socket.io")(server);
 	const request = require("request");
